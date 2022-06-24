d9fae4692e1a4a1fb9c5145dff1f1eac230c3063
saanhvisrivastava <63591636+saanhvisrivastava@users.noreply.github.com>
2022-06-24 11:36:45 +0530

    Add files via upload

---
+++ Shoot.js
@@ -0,0 +1,71 @@
+AFRAME.registerComponent('bullets',{
+
+    init:function(){
+        this.shootBullet()
+        
+    },
+    shootBullet:function(){
+        window.addEventListener('keydown',(e)=>{
+            if(e.key==='z'){
+                var bullet=document.createElement('a-entity')
+                bullet.setAttribute('geometry',{
+                    primitive:'sphere',
+                    radius:0.1
+                })
+                bullet.setAttribute('material','color','black')
+                bullet.setAttribute('dynamic-body',{
+                    shape:'sphere',
+                    mass:'0'
+                })
+                var cam=document.querySelector('#camera')
+                pos=cam.getAttribute('position')
+                bullet.setAttribute('position',{
+                    x:pos.x,
+                    y:pos.y,
+                    z:pos.z
+                })
+               // bullet.setAttribute('velocity',{
+                   // x:0,
+                   // y:0,
+                   // z:-1
+               // }
+               //)
+
+
+                var camera=document.querySelector('#camera').object3D
+                var direction=new THREE.Vector3()//object created
+                camera.getWorldDirection(direction)
+                bullet.setAttribute('velocity',direction.multiplyScalar(-10))//add the velocity
+
+                var scene=document.querySelector('#scene')
+
+                bullet.addEventListener('collide',this.removeBullet)
+
+                scene.appendChild(bullet)
+            }
+
+        })
+    },
+
+    removeBullet:function(e){
+        console.log(e.detail.target.el)
+        console.log(e.detail.body.el)
+        var element=e.detail.target.el
+        var elementHit=e.detail.body.el
+        if(elementHit.id.includes('box')){
+            elementHit.setAttribute('material',{
+                opacity:0.6,
+                transparent:true
+            })
+        }
+
+        var impulse=new CANNON.Vec3(-2, 2 ,1);
+        var worldPoint=new CANNON.Vec3().copy(elementHit.getAttribute('position'))
+        elementHit.body.applyImpulse(impulse,worldPoint)
+        element.removeEventListener('collide',this.shootBullet)
+        var scene=document.querySelector('#scene')
+        scene.removeChild(element)
+
+    }
+})
+
